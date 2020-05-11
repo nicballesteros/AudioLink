39f7fc63a94b4e9882ed9e076913915b0ab569a2
nicballesteros <nicballesteros2@gmail.com>
2020-05-10 22:12:17 -0400

    Added circular array to Client so that server would no longer give an error

--- Client/src/Call.java
+++ Client/src/Call.java
@@ -1,3 +1,4 @@
+import javax.sound.sampled.AudioFormat;
 import java.io.Serializable;
 import java.util.Objects;
 
@@ -6,6 +7,20 @@ public class Call implements Serializable {
     private User sender;
     private int type;
     private boolean accepted;
+    private CircularArray senderQueue;    //sender's mic
+    private CircularArray recipientQueue; //recipient's mic
+    private AudioFormat format;
+
+    public static final Object senderKey = new Object();
+    public static final Object recipientKey = new Object();
+
+    public AudioFormat getFormat() {
+        return format;
+    }
+
+    public void setFormat(AudioFormat format) {
+        this.format = format;
+    }
 
     public void setType(int type) {
         this.type = type;
@@ -15,6 +30,7 @@ public class Call implements Serializable {
         this.recipient = to;
         this.sender = from;
         this.type = type;
+        accepted = false;
     }
 
     public User getRecipient() {
@@ -44,6 +60,46 @@ public class Call implements Serializable {
         Call call = (Call) o;
         return type == call.type &&
                 Objects.equals(recipient, call.recipient) &&
+                Objects.equals(sender, call.sender) ||
+                type != call.type &&
+                        Objects.equals(sender, call.recipient) &&
+                        Objects.equals(recipient, call.sender);
+    }
+
+    public boolean equalsExact(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Call call = (Call) o;
+        return type == call.type &&
+                Objects.equals(recipient, call.recipient) &&
                 Objects.equals(sender, call.sender);
+    }
+
+    public void writeToQueue(int queueNumber, byte b) {
+        if(queueNumber == 0) { //sender queue
+            synchronized (senderKey) {
+                senderQueue.write(b);
+            }
+        } else if (queueNumber == 1) { //recipient queue
+            synchronized (recipientKey) {
+                recipientQueue.write(b);
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public byte readFromQueue(int queueNumber) {
+        if(queueNumber == 0) { //sender queue
+            synchronized (senderKey) {
+                return senderQueue.read();
+            }
+        } else if (queueNumber == 1) { //recipient queue
+            synchronized (recipientKey) {
+                return recipientQueue.read();
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
